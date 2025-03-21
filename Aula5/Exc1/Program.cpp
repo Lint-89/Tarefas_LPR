8937a18cf19bff666c2d024e6630dee7066d04b8
guih_vb <guilherme11vilasboas@gmail.com>
2025-03-21 09:36:28 -0300

    Aula5

---
+++ Aula5/Exc1/Program.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    
+  
+    cout << "Digite números inteiros" << endl;
+
+
+}
