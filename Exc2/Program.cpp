f0d5ec484c32a1bc2f4b36e4c3475ff3dd4103aa
guih_vb <guilherme11vilasboas@gmail.com>
2025-03-28 17:33:30 -0300

    sim

---
+++ Exc2/Program.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cout << "Digite dois números inteiros: ";
+    cin >> A >> B;
+    
+    if (A % B == 0 || B % A == 0) {
+        cout << "Eles são multiplos" << endl;
+    } else {
+        cout << "Eles não são múltiplos" << endl;
+    }
+    
+    return 0;
+}
---
+++ Exc3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Escolha uma classe de personagem:");
+        Console.WriteLine("1 - Guerreira");
+        Console.WriteLine("2 - Mago");
+        Console.WriteLine("3 - Arqueira");
+        
+        Console.Write("Digite o número da sua classe: ");
+        int escolha = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("\nHabilidades Especiais:");
+        if(escolha == 1)
+        {
+            Console.WriteLine("- Ataque Pesado");
+            Console.WriteLine("- Defesa Total");
+        }
+        else if(escolha == 2)
+        {
+            Console.WriteLine("- Bola de Fogo");
+            Console.WriteLine("- Escudo de Gelo");
+        }
+        else if(escolha == 3)
+        {
+            Console.WriteLine("- Flecha Precisa");
+            Console.WriteLine("- Disparo Triplo");
+        }
+        else
+        {
+            Console.WriteLine("Escolha um número válido de 1 a 3");
+        }
+        
+           
+        
