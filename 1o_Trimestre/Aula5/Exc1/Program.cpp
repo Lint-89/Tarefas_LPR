0a58f9d6e6f02f103df0f624234a18f01e8de78c
guih_vb <guilherme11vilasboas@gmail.com>
2025-06-06 22:02:38 -0300

    teste

---
+++ 1o_Trimestre/Aula5/Exc1/Program.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    
+    int quantidade, numero, contador = 0;
+    double media, soma = 0;
+    cout << "Digite a quantidade de números que serão digitados" << endl;
+    cin >> quantidade;
+
+    int i = 0;
+        while(i<quantidade){
+            cout <<"Digite um número" << endl;
+            cin >> numero;
+            i++;
+            if(numero%2==0){
+                soma += numero;
+                contador++;
+            }
+        }
+        
+        
+        if(contador>0){
+            media = soma/contador;
+        cout << "A média dos números pares é: " << media << endl;
+    } else {
+        cout << "Nenhum número par foi digitado." << endl;
+        }
+
+    }
---
+++ 1o_Trimestre/Aula5/Exc2/Program.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <locale>
+#include <random>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    int contador = 0, numero;
+    random_device numAleatorio ;
+uniform_int_distribution<> distribuicao(1,100);
+int valorInteiro = distribuicao(numAleatorio);
+
+do{ 
+    cout << "Digite um valor que você ache que é o número ";
+    cin >> numero;
+    contador ++;
+
+    if(numero<valorInteiro){
+    cout << "Chutou baixo" << endl;
+}else if(numero>valorInteiro){
+    cout << "Chutou alto" << endl;
+}else{
+    cout << "Acertou" << endl;
+}
+
+} while (numero != valorInteiro);
+
+
+
+if(numero=valorInteiro){
+    cout <<" Você acertou em " << contador << " tentativas e o número era " << valorInteiro << endl;
+}
+
+
+}
