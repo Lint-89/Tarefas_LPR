fc650f65b1d712418bc2ae978642ded68bb8a705
guih_vb <guilherme11vilasboas@gmail.com>
2025-06-06 21:55:52 -0300

    teste

---
+++ 1o_Trimestre/Aula5/Exc4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Insira um número inteiro");
+ int numero = int.Parse(Console.ReadLine());
+
+ int quadrado = numero * numero;
+        int soma = 0;
+    while(quadrado>0){
+
+        soma += quadrado % 10; 
+          quadrado /=10;
+        }
+        Console.WriteLine("A soma dos dígitos do quadrado do número é: " + soma);
+    
+        
+    
